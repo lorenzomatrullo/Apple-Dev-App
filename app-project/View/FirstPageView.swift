66b65f7a1f2e7cecbe2de28b11a661aa338904ab
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2024-10-28 11:52:36 +0100

    adding personal device to run the app. adding text-to-speech feature as Welcome Message fixed the always repeated text-to-speech Welcome Message every time accessing the main page through the implementation of hasSpokenWelcomeMessage() function

--- app-project/View/FirstPageView.swift
+++ app-project/View/FirstPageView.swift
@@ -1,8 +1,11 @@
 import Foundation
 import SwiftUI
+import AVFoundation
 
 struct FirstPageView: View {
     @EnvironmentObject var model: Model
+    private let speechSynthesizer = AVSpeechSynthesizer()
+    @State private var hasSpokenWelcomeMessage = false  // Track if the message has been spoken
     
     var body: some View {
         NavigationView {
@@ -20,7 +23,22 @@ struct FirstPageView: View {
                 .foregroundColor(.primary)
                 .padding(.top, 15)
             )
+            .onAppear {
+                if !hasSpokenWelcomeMessage {
+                    speakWelcomeMessage()
+                    hasSpokenWelcomeMessage = true  // Update the state to prevent future calls
+                }
+            }
         }
+    }
+    
+    private func speakWelcomeMessage() {
+        let welcomeMessage = "Welcome to the Recipe App! Explore delicious recipes."
+        let utterance = AVSpeechUtterance(string: welcomeMessage)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        // utterance.rate = 0.5
+        
+        speechSynthesizer.speak(utterance)
     }
 }
 
